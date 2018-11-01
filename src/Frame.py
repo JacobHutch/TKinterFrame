0fec67d201cbb52de9147d9ffe72f5d0d0db2b32
Jacob Hutchison <jacobhutch.450@gmail.com>
2018-11-01 15:31:54 -0400

    * Added a lot of stuff P.S. I'm really bad at commit messages sorry

--- src/Frame.py
+++ src/Frame.py
@@ -1,6 +1,8 @@
 '''
-Attributes:
+!- Keep in mind I have no idea what I'm doing in terms of writing
+	professionally clean code -!
 
+Options:
 name - string : 'Title'
 size - tuple  : (x,y)
 bg   - string : '#xxxxxx'
@@ -10,14 +12,21 @@ res  - tuple  : (bool,bool)
 import tkinter as tk
 
 class App:
-    def __init__(self,**options):
-        self.name = options.get('name', 'Test')
-        self.size = options.get('size',(100,100))
-        self.bg = options.get('bg','#000000')
-        self.res = options.get('res',(0,0))
+	def __init__(self,**options):
 
-        self.ui = tk.Tk()
-        self.ui.geometry(str(self.size[0])+'x'+str(self.size[1]))
-        self.ui['background'] = self.bg
-        self.ui.resizable(self.res[0],self.res[1])
-        tk.mainloop()
+		#Option variables initialization
+		self.name = options.get('name', 'Test')
+		self.size = options.get('size',(100,100))
+		self.bg = options.get('bg','#000000')
+		self.res = options.get('res',(0,0))
+
+		#Main window creation
+		self.ui = tk.Tk()
+		self.ui.title(self.name)
+		self.ui.geometry(str(self.size[0])+'x'+str(self.size[1]))
+		self.ui['background'] = self.bg
+		self.ui.resizable(self.res[0],self.res[1])
+
+	def bindKeys(self,**keyBindList):
+		for char in keyBindList:
+			self.ui.bind_all('<'+char+'>',keyBindList[char])
--- src/Game.py
+++ src/Game.py
@@ -1,4 +1,34 @@
-import Frame as tkf
+'''
+Concept: A top-down walking simulator
 
-options={'name':'hg','res':(1,1),'bg':'#123456','size':(500,500)}
-app = tkf.App(**options)
+So I think what I'm gonna do is have the frame app, which the game app will pass
+through as an argument
+Ex:
+From Frame import App
+class test:
+	def __init__(self, Frame)
+'''
+
+import Frame as tkf
+import time as t
+
+appOptions={'name':'Potential Walking Simulator',
+			'res':(1,1),'bg':'#123456','size':(500,500)}
+
+class Game:
+	def __init__(self, App):
+		self.frame = App
+
+		#Dict format: 'char':function
+		self.keyBinds={'q':self.kill,'t':self.bgColor}
+		self.frame.bindKeys(**self.keyBinds)
+
+		self.frame.ui.mainloop()
+
+	def kill(self, event):
+		self.frame.ui.destroy()
+
+	def bgColor(self, event):
+		self.frame.ui['background']='#'+''.join(str(t.time()*10**6)[-8:-2])
+
+test = Game(tkf.App(**appOptions))
